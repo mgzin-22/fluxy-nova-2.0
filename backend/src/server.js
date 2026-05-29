559990f92df1aaaf91ff7c4ece9835765d4b378e
mgzin-22 <miguelfsa738@gmail.com>
2026-05-28 22:46:38 -0300

    Aumenta limite de upload de imagem

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,7 +6,8 @@ const app = express();
 
 //  PRIMEIRO vem os middlewares
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
 //  depois as rotas
 const authRoutes = require("./routes/auth.routes");
